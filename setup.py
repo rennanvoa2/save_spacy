e4f95747f96aeb3c19cceca35ff53fb7b23f9f89
rennanvoa2 <rennanvoa2@lwmail.com.br>
2022-08-30 05:45:16 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'save_spacy',
+    packages = ['save_spacy'],
+    version = 'v1.0.0',
+    description = "Save and Load Spacy models locally and on GCP.",
+    author="Rennan Valadares",
+    author_email="rennanvoa2@gmail.com",
+    url = 'https://github.com/rennanvoa2/',
+    download_url = 'https://github.com/rennanvoa2/ner_evaluator/archive/refs/tags/V1.0.0.tar.gz',
+    keywords = ['Spacy', 'spacy', 'save model', 'nlp'],
+    classifiers = [],
+)
